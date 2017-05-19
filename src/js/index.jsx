05236f377978bb23e33b36dcaa4206ed090d2357
Greg Pascale <greg.pascale@gmail.com>
2017-05-19 16:50:46 -0700

    add redux and up some versions

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Router, Route, IndexRoute } from 'react-router';
+import { createBrowserHistory } from 'history';
+import todoApp from '../reducers';
 
-import App from '../components/hello/Hello';
+import App from '../components/app/App';
 
 require('../scss/bundle.scss');
 
+let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
-    <Route path="/" component={App}>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={createBrowserHistory()}>
+      <Route path="/" render={() => <App store={store}/>}/>
+    </Router>
+  </Provider>
   , document.getElementById('react-root'));
