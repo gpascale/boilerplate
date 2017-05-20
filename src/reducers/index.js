d016b61a8c61e195cc2e43635c2c129f1a5aec45
Greg Pascale <greg.pascale@gmail.com>
2017-05-19 17:06:29 -0700

    add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+};
+
+function mainReducer(state = initialState, action) {
+  switch (action.type) {
+    // handle different types of actions
+    default:
+      return state;
+  }
+}
+
+export default mainReducer;
